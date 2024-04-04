780c2978ad9780a3eff8816d3ae671ce2bf711e0
Victiniiiii <victinii494@gmail.com>
2024-04-04 20:43:16 +0300

    updates

--- compost.js
+++ compost.js
@@ -1,3 +1,32 @@
+//compost.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set the views directory
+app.set('views', path.join(__dirname, ''));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'static' directory
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+// Render compost.ejs
+app.get('/', async (req, res) => {
+    try {
+        res.render('compost', {});
+    } catch (error) {
+        console.error('Error rendering template:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 async function bazaarconnect() {
     const response = await fetch('https://api.hypixel.net/v2/skyblock/bazaar');
     const data = await response.json();
@@ -733,5 +762,6 @@ async function compostspreadsheet() {
     }
 }
 
+bazaarconnect();
 getbestmatterandfuel();
 compostspreadsheet();
